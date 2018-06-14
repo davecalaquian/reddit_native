f817a4f955725fad68af280f85d5fd00a43da049
cedrick95 <36392647+cedrick95@users.noreply.github.com>
2018-06-14 16:22:55 +0800

    app update

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, StatusBar, TouchableOpacity , Image} from 'react-native';
-
-// import { Icon } from 'react-native-elements';
-// import CardSection from './src/components/CardSection';
-import Welcome from './src/components/Welcome';
-
 
 export default class App extends React.Component {
   render() {
@@ -15,7 +9,7 @@ export default class App extends React.Component {
            barStyle="default"
         />
 
-        <Welcome />
+
 
       </View>
     );
@@ -28,23 +22,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     flexDirection: 'column',
     marginTop: StatusBar.currentHeight,
-  },
-  addWidgetStyle: {
-    position: 'absolute',
-    right: 20,
-    bottom: 75
   }
 });
-
-// <Header />
-// <ScrollView>
-//   <CardSection/>
-//   <CardSection/>
-//   <CardSection/>
-// </ScrollView>
-// <TouchableOpacity style={styles.addWidgetStyle}>
-// <Image
-//     source={require('./src/assets/image/addButton.png')}
-//     style={{ width: 54, height: 54 }}
-//     />
-// </TouchableOpacity>
