edad5a6108cc2aa9ef3eaeb2f6d7c29249b3377d
Dave Calaquian <dave.calaquian@whitecloak.com>
2018-06-19 10:33:41 +0800

    Adjusted Margin Bottom

--- src/components/CardFooter.js
+++ src/components/CardFooter.js
@@ -20,7 +20,7 @@ const styles = {
     borderBottomLeftRadius: 9,
     borderBottomRightRadius: 9,
     marginLeft: 10,
-    marginBottom: 5,
+    marginBottom: 13,
     marginRight: 10,
     paddingLeft: 5,
     paddingRight: 5,
