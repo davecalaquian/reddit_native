2315f51b923f4d7372e6a2c6c6c86e2716daf7eb
Dave Calaquian <dave.calaquian@whitecloak.com>
2018-06-21 12:55:27 +0800

    Fixed Small Card API call and design

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,23 +7,200 @@ import CardSection from './CardSection';
 import SmallCard from './SmallCard';
 
 class Home extends React.Component {
-
-  state = {
-    redditPost: [],
-    redditUser: '',
-    loading: true
-  };
+  constructor() {
+    super();
+    this.state = {
+      redditPost: [],
+      redditUser: '',
+      loading: true,
+      popularSubs: []
+    };
+  }
 
   componentWillMount() {
     axios.get('https://reddit.com/r/all.json').then(res => {
       this.setState({
-        redditPost: res.data.data.children,
-        loading: false
+        redditPost: res.data.data.children
       });
     }
     );
 
+    axios.get('https://www.reddit.com/subreddits.json').then(res => {
+      this.setState({
+        popularSubs: res.data.data.children,
+        loading: false
+      });
+    });
   }
+
+
+  renderSmallCard() {
+      return (
+        <View
+          style={{
+            flexDirection: 'row',
+            marginRight: 10,
+            marginLeft: 10,
+            marginBottom: 10
+          }}
+        >
+          <View style={{ flex: 1, flexDirection: 'column', marginRight: 10 }}>
+            <View style={{ flex: 1, marginBottom: 10 }}>
+              <SmallCard
+                publicDescription={this.state.popularSubs[0].data.public_description}
+                subreddit={this.state.popularSubs[0].data.url}
+                imageSource={this.state.popularSubs[0].data.icon_img}
+                subscribers={this.state.popularSubs[0].data.subscribers}
+                viewStyle={{
+                  height: 148,
+                  backgroundColor: '#0074CD',
+                  borderRadius: 8,
+                  padding: 20
+                 }}
+                 imageStyle={{
+                   height: 28,
+                   width: 28,
+                   overflow: 'hidden',
+                   borderRadius: 50
+                 }}
+                 subStyle={{
+                   color: '#fff',
+                   fontSize: 12,
+                   letterSpacing: 0.5,
+                   lineHeight: 18
+                 }}
+                 textStyle={{
+                   color: '#fff',
+                   letterSpacing: 0,
+                   fontSize: 12
+                 }}
+                 urlStyle={{
+                   color: '#fff',
+                   letterSpacing: 0,
+                   lineHeight: 22,
+                   fontSize: 12,
+                   flex: 1
+                 }}
+                 countStyle={{
+                   color: '#fff',
+                   fontSize: 9,
+                   opacity: 0.5,
+                   flex: 1
+                 }}
+              />
+            </View>
+
+            <View style={{ flex: 1 }}>
+              <SmallCard
+                publicDescription={this.state.popularSubs[1].data.public_description}
+                subreddit={this.state.popularSubs[1].data.url}
+                imageSource={this.state.popularSubs[1].data.icon_img}
+                subscribers={this.state.popularSubs[1].data.subscribers}
+                viewStyle={{
+                  height: 148,
+                  backgroundColor: '#FFD93E',
+                  borderRadius: 8,
+                  padding: 20
+                 }}
+                 imageStyle={{
+                   height: 28,
+                   width: 28,
+                   overflow: 'hidden',
+                   borderRadius: 50
+                 }}
+                 subStyle={{
+                   color: '#000',
+                   fontSize: 12,
+                   letterSpacing: 0.5,
+                   lineHeight: 18
+                 }}
+                 textStyle={{
+                   color: '#000',
+                   letterSpacing: 0,
+                   fontSize: 12
+                 }}
+                 urlStyle={{
+                   color: '#000',
+                   letterSpacing: 0,
+                   lineHeight: 22,
+                   fontSize: 12,
+                   flex: 1
+                 }}
+                 countStyle={{
+                   color: '#000',
+                   fontSize: 9,
+                   opacity: 0.5,
+                   flex: 1
+                 }}
+              />
+            </View>
+
+          </View>
+
+          <View style={{ flex: 1, flexDirection: 'column' }}>
+            <View style={{ flex: 1 }}>
+              <View>
+                <Image
+                  style={{
+                    height: 153,
+                    borderTopLeftRadius: 8,
+                    borderTopRightRadius: 8
+                }}
+                  source={{ uri: this.state.popularSubs[2].data.icon_img }}
+                />
+              </View>
+            </View>
+            <View style={{ flex: 1 }}>
+              <SmallCard
+                publicDescription={this.state.popularSubs[2].data.public_description}
+                subreddit={this.state.popularSubs[2].data.url}
+                imageSource={this.state.popularSubs[2].data.icon_img}
+                subscribers={this.state.popularSubs[2].data.subscribers}
+                viewStyle={{
+                  height: 155,
+                  backgroundColor: '#fff',
+                  borderBottomLeftRadius: 8,
+                  borderBottomRightRadius: 8,
+                  padding: 20
+                 }}
+                 imageStyle={{
+                   height: 28,
+                   width: 28,
+                   overflow: 'hidden',
+                   borderRadius: 50
+                 }}
+                 subStyle={{
+                   color: '#000',
+                   fontSize: 12,
+                   letterSpacing: 0.5,
+                   lineHeight: 18
+                 }}
+                 textStyle={{
+                   color: '#000',
+                   letterSpacing: 0,
+                   fontSize: 12
+                 }}
+                 urlStyle={{
+                   color: '#000',
+                   letterSpacing: 0,
+                   lineHeight: 22,
+                   fontSize: 12,
+                   flex: 1
+                 }}
+                 countStyle={{
+                   color: '#000',
+                   fontSize: 9,
+                   opacity: 0.5,
+                   flex: 1
+                 }}
+              />
+            </View>
+          </View>
+
+        </View>
+          );
+    }
+
 
   renderContent() {
     let count = 0;
@@ -35,52 +212,13 @@ class Home extends React.Component {
             return <CardSection key={redditPost.data.name} data={redditPost} styleType={1} />;
 
           case 2:
-            return <CardSection key={redditPost.data.name} data={redditPost} styleType={2}  />;
-          //
-          // case 3:
-          //   return (
-          //     <View key={redditPost.data.name} style={{ flexDirection: 'column', width: 176 }}>
-          //       <SmallCard
-          //       data={redditPost}
-          //       style={3}
-          //       subreddit={redditPost.data.subreddit}
-          //       />
-          //     </View>
-          //   );
+            return <CardSection key={redditPost.data.name} data={redditPost} styleType={2} />;
 
-          case 4:
+          case 3:
             return (
-              //Container
-              <View
-                key={redditPost.data.name}
-                style={{
-                  flexDirection: 'row',
-                  marginRight: 10,
-                  marginLeft: 10,
-                  marginBottom: 10
-                }}
-              >
-                <View style={{ flex: 1 }}>
-                  <View style={{ flex: 1, marginBottom: 10, marginRight: 10 }}>
-                    <SmallCard data={redditPost} style={1} subreddit={redditPost.data.subreddit} />
-                  </View>
-
-                  <View style={{ flex: 1, marginRight: 10 }}>
-                    <SmallCard data={redditPost} style={2} subreddit={redditPost.data.subreddit} />
-                  </View>
-                </View>
-
-                <View style={{ flex: 1 }}>
-                  <View style={{ flex: 1 }}>
-                    <SmallCard data={redditPost} style={4} subreddit={redditPost.data.subreddit} />
-                  </View>
-
-                  <View style={{ flex: 1 }}>
-                    <SmallCard data={redditPost} style={3} subreddit={redditPost.data.subreddit} />
-                  </View>
-                </View>
-              </View>
-            );
+              <View key={redditPost.data.name}>
+                {this.renderSmallCard()}
+              </View>);
 
           case 5:
             return <CardSection key={redditPost.data.name} data={redditPost} styleType={5} />;
@@ -92,8 +230,10 @@ class Home extends React.Component {
       // else{
       //   count = 0;
       // }
-    });
-  }
+    }
+  );
+}
+
 
   render() {
     return (
